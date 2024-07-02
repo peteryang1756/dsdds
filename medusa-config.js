1b0c39ccb91b09e32f52d4bacfc6df14e5c0e6fe
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-07-02 11:36:14 +0800

    Update medusa-config.js

--- medusa-config.js
+++ medusa-config.js
@@ -19,23 +19,17 @@ switch (process.env.NODE_ENV) {
 
 try {
   dotenv.config({ path: process.cwd() + "/" + ENV_FILE_NAME });
-} catch (e) {
-  console.error(`Failed to load environment variables from ${ENV_FILE_NAME}:`, e);
-}
+} catch (e) {}
 
 // CORS when consuming Medusa from admin
-const ADMIN_CORS = process.env.ADMIN_CORS || "https://dsdds.vercel.app";
-
-const OAuth2AuthorizationURL = process.env.OAUTH2_AUTHORIZATION_URL || "";
-const OAuth2TokenURL = process.env.OAUTH2_TOKEN_URL || "";
-const OAuth2ClientId = process.env.OAUTH2_CLIENT_ID || "";
-const OAuth2ClientSecret = process.env.OAUTH2_CLIENT_SECRET || "";
-const OAuth2Scope = process.env.OAUTH2_SCOPE || "";
+const ADMIN_CORS =
+  process.env.ADMIN_CORS || "https://dsdds.vercel.app";
 
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
 
-const DATABASE_URL = process.env.DATABASE_URL || "postgres://localhost/medusa-starter-default";
+const DATABASE_URL =
+  process.env.DATABASE_URL || "postgres://localhost/medusa-starter-default";
 
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 
@@ -57,32 +51,6 @@ const plugins = [
         open: process.env.OPEN_BROWSER !== "false",
       },
     },
-  },
-  {
-    resolve: "medusa-plugin-auth",
-    /** @type {import('medusa-plugin-auth').AuthOptions} */
-    options: [
-      {
-        type: "oauth2",
-        strict: "all",
-        identifier: "oauth2",
-        authorizationURL: OAuth2AuthorizationURL,
-        tokenURL: OAuth2TokenURL,
-        clientID: OAuth2ClientId,
-        clientSecret: OAuth2ClientSecret,
-        scope: OAuth2Scope.split(","),
-        admin: {
-          callbackUrl: `${process.env.BACKEND_URL}/admin/auth/oauth2/cb`,
-          failureRedirect: `${process.env.ADMIN_URL}/login`,
-          successRedirect: `${process.env.ADMIN_URL}/`
-        },
-        store: {
-          callbackUrl: `https://dsdds-1adm.onrender.com/store/auth/oauth2/cb`,
-          failureRedirect: `https://dsdds-1adm.onrender.com/login`,
-          successRedirect: `https://shop.ssangyongsports.eu.org/`
-        },
-      },
-    ],
   },
 ];
 
@@ -109,11 +77,14 @@ const projectConfig = {
   database_url: DATABASE_URL,
   admin_cors: ADMIN_CORS,
   redis_url: process.env.REDIS_URL,
-  database_extra: process.env.NODE_ENV !== "development" ? {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  } : {},
+  // Uncomment the following lines to enable REDIS
+  // redis_url: REDIS_URL
+  database_extra: process.env.NODE_ENV !== "development" ? 
+    {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    } : {},
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule} */
